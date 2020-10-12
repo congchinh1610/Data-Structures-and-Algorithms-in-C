3602e1ef9c214d939468ab4e2d71c2a5850f0fa2
Nguyen Chinh <congchinh1610@gmail.com>
2020-10-12 21:15:41 +0700

    init stack

---
+++ 03. Stack/Stack -  Exercises.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct SinhVien {
+  int maSV;
+  char hoTen[25];
+  float dLT, dTH, dT, dTB;
+};
+
+struct Node {
+  SV info;
+  struct Node *next;
+};
+
+struct Stack {
+  Node top;
+};
+
+void init(Stack &s) {
+  s.top = NULL;
+}
+
+void isEmpty(Stack s) {
+  if(s.top == NULL)
+    return 0;
+  else
+    return 1;
+}
+
+
+
+int main() {
+
+
+}
